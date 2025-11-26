48baa7be761e0c07dd927e2131ce23d5ca3c946a
tusky <isaac@dw-perspective.org.uk>
2025-11-26 16:10:49 -0500

    Fix(stopped overcomplicating channel_to_df()

--- utils/ps5.py
+++ utils/ps5.py
@@ -8,14 +8,9 @@ def channel_to_df(channel: pd.Series) -> pd.DataFrame:
     For quick plotting, calculates the length which most columns share in common, 
     converts this to a dataframe.
     """
-    lengths = []
-    for item in channel:
-        lengths.append(len(item))
+    unwanted = ['edges','peak_locs', 'energy']
 
-    count = Counter(lengths)
-    desired_len = count.most_common()[0][1]
-
-    same_lengths = channel[[item for item in channel.keys() if len(item) == desired_len]]
+    same_lengths = channel[[item for item in channel.keys() if item not in unwanted]]
     df = pd.DataFrame(
         data = same_lengths.to_dict()
     )
